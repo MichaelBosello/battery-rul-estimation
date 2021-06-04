Fix capacity calculation and value smoothing

battery-rul-estimation/data_processing/nasa_random_data.py
@@ -9,6 +9,7 @@ import scipy.io
 DATA_PATH = 'data/nasa-randomized/'
 NOMINAL_CAPACITY = 2.2
 
+
 class NasaRandomizedData():
     def __init__(self, base_path="./"):
         self.path = base_path + DATA_PATH
@@ -40,6 +41,8 @@ class NasaRandomizedData():
     def _get_data(self, names):
         cycle_x = []
         cycle_y = []
+        first_y = True
+        y_between_count = 0
         battery_n_cycle = []
         time = []
         current = []
@@ -71,20 +74,33 @@ class NasaRandomizedData():
                     np.hstack(cycle.loc[cycle["cycle"] == x, "temperature"].to_numpy().flatten()).flatten()]))
 
                 n_cycles += 1
-                step_time = np.hstack(cycle.loc[cycle["cycle"] == x, "relativeTime"].to_numpy().flatten()).flatten()
+                step_time = np.hstack(cycle.loc[cycle["cycle"] == x, "time"].to_numpy().flatten()).flatten()
                 time.append(step_time / 3600)
                 current.append(np.hstack(cycle.loc[cycle["cycle"] == x, "current"].to_numpy().flatten()).flatten())
                 max_step = max([max_step, cycle_x[-1].shape[0]])
 
-                if cycle_y == []:
+                if (cycle.loc[cycle["cycle"] == x, "comment"].iloc[0] == "reference discharge" and
+                     (x < 2 or cycle.loc[cycle["cycle"] == x-2, "comment"].iloc[0] != "reference discharge")):
+                    current_y = np.trapz(current[-1], np.hstack(cycle.loc[cycle["cycle"] == x, "time"].to_numpy().flatten()).flatten())/3600
+                    if y_between_count > 0:
+                        if first_y is True:
+                            step_y = 0
+                        else:
+                            step_y = (cycle_y[-1] - current_y)/y_between_count
+                        while y_between_count > 0:
+                            cycle_y.append(cycle_y[-1]-step_y)
+                            y_between_count -=1
+                    cycle_y.append(current_y)
+                elif first_y is True:
+                    while y_between_count > 0:
+                        cycle_y.append(cycle_y[-1])
+                        y_between_count -=1
                     cycle_y.append(NOMINAL_CAPACITY)
-                elif (cycle.loc[cycle["cycle"] == x, "comment"].iloc[0] == "reference discharge" and
-                     cycle.loc[cycle["cycle"] == x-2, "comment"].iloc[0] != "reference discharge"):
-                    cycle_y.append(np.trapz(current[-1], time[-1]))
                 else:
-                    cycle_y.append(cycle_y[-1])
+                    y_between_count += 1
+                first_y = False
 
-
+            first_y = True
             battery_n_cycle.append(n_cycles)
 
         cycle_x = self._to_padded_numpy(cycle_x, [len(cycle_x), max_step, len(cycle_x[0][0])])